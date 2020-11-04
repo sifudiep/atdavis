58ca572050b9bf2a4971e547a29c1c919f54b2f0
sifudiep <code.sifudiep@gmail.com>
2020-11-04 19:27:51 +0100

    changed to davisid bug

--- bot.js
+++ bot.js
@@ -5,8 +5,6 @@ bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
 });
 
-imageURL = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.premierecreative.com%2Fwp-content%2Fuploads%2F2012%2F11%2Furlstructure.jpg&f=1&nofb=1";   
-
 davisid = "231432827139391488";
 mikeid = "180735443418087424";
 
@@ -15,25 +13,27 @@ bot.on('message', msg => {
 
     rnd = Math.floor((Math.random() * 2) + 1);
 
-    davisImg = new Discord.MessageAttachment;
-    if (rnd === 1) {
-        davisImg.attachment = "./mrandmrs.png";
-    }
+
 
     if (msg.author.id === davisid) {
         msg.react('772820533066203167')
     }
 
+    davisImg = new Discord.MessageAttachment;
+    davisImg.attachment = "https://i.postimg.cc/zXmZVfZ5/mrandmrs.png";
+
     if (msg.content === "testdb") {
+        console.log("oopsie");
         msg.channel.send(davisImg);
     }
 
     msg.mentions.users.forEach(user => {
-        if (user.id === mikeid) {
+        if (user.id === davisid) {
             console.log("sending davis img...")
             msg.channel.send(davisImg)
         }
     });
+    
 })
 
 bot.login(process.env.token);
