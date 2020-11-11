3bd74afcec4902aa51beb5549d402a96f2f21746
sifudiep <code.sifudiep@gmail.com>
2020-11-11 19:31:16 +0100

    fixed duckduckgo clown scraping and sending to discord channel

--- bot.js
+++ bot.js
@@ -4,25 +4,100 @@ const bot = new Discord.Client();
 const Nightmare = require('nightmare')
 const nightmare = Nightmare();
 
-imageURLS = [];
-yValue = 0
+const fs = require('fs')
+const request = require('request')
+
+let imageURLS = [];
+let yValue = 0
+const path = "./clown.jpg"
+
+const download = (url, path, callback) => {
+  request.head(url, (err, res, body) => {
+    request(url)
+      .pipe(fs.createWriteStream(path))
+      .on('close', callback)
+  })
+}
+
+function startBot() {
+    bot.on('ready', () => {
+        console.log(`Logged in as ${bot.user.tag}!`);
+    });
+
+    davisid = "231432827139391488";
+    mikeid = "180735443418087424";
+    danielid = "157188647001522176";
+
+    bot.on('message', msg => {
+        if (msg.author.bot) return;
+
+        if (msg.author.id === davisid || msg.author.id === danielid) {
+            msg.react('772820533066203167');
+            msg.react('773656627281920091');
+        }
+
+        danielImg = new Discord.MessageAttachment;
+        danielImg.attachment = "./danielclown.jpg"
+
+        davisImg = new Discord.MessageAttachment;
+
+        if (msg.content === "testdb") {
+            console.log(imageURLS[clownIndex])
+            download(imageURLS[clownIndex], path , (err) => {
+                console.log("wow it downloaded....")
+                davisImg.attachment = path;
+                msg.channel.send(davisImg)
+                clownIndex += 1
+            })
+        }
+
+
+        msg.mentions.users.forEach(user => {
+            console.log("download start...")
+            if (user.id === davisid) {
+                download(imageURLS[clownIndex], path, () => {
+                    davisImg.attachment = path;
+                    msg.channel.send(davisImg)
+                    clownIndex += 1;
+                })
+            }
+            
+            if (user.id === danielid) {
+                console.log("sending daniel img..")
+                msg.channel.send(danielImg)
+            }
+        });
+    
+    })
+
+    bot.login(process.env.token);
+
+}
+
+console.log("STARTING NIGHTMARE...")
 
 nightmare.goto("https://duckduckgo.com/?q=clown&atb=v231-1&iar=images&iax=images&ia=images")
-for (i = 0; i < 10; i++) {
-    yValue += 99999
-    nightmare.scrollTo(yValue, 0)
-    nightmare.wait(1000)
-}
+// for (i = 0; i < 10; i++) {
+//     yValue += 99999
+//     nightmare.scrollTo(yValue, 0)
+//     nightmare.wait(1000)
+// }
 nightmare.evaluate(() => {
         imgElements = document.body.getElementsByClassName("tile--img__img  js-lazyload");
         imgURLS = []
-        for (let index = 0; index < imgElements.length; index++) {
-            imgURLS.push(imgElements[index].src)
+
+        for (let i = 0; i < imgElements.length; i++) {
+            imgURLS.push(imgElements[i].src)
         }
         return imgURLS;
     })
     .then(result => {
         imageURLS = result;
+        console.log("imageURLS LENGTH: ", imageURLS.length)
+        console.log("nightmare is finished!!!")    
+        console.log("Starting bot...")
+        console.log("RESULT[0]", result[0])
+        startBot();
     })
     .catch(error => {
         console.error('Search failed:', error)
@@ -31,45 +106,3 @@ nightmare.end();
 
 clownIndex = 0;
 
-bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
-});
-
-davisid = "231432827139391488";
-mikeid = "180735443418087424";
-danielid = "157188647001522176";
-
-
-
-bot.on('message', msg => {
-    if (msg.author.bot) return;
-
-    if (msg.author.id === davisid || msg.author.id === danielid) {
-        msg.react('772820533066203167');
-        msg.react('773656627281920091');
-    }
-
-    davisImg = new Discord.MessageAttachment;
-    davisImg.attachment = imageURLS[clownIndex];
-
-    if (msg.content === "testdb") {
-        msg.channel.send(davisImg);
-        clownIndex += 1;
-    }
-
-    msg.mentions.users.forEach(user => {
-        if (user.id === davisid) {
-            console.log("sending davis img...")
-            msg.channel.send(davisImg)
-            clownIndex += 1;
-        }
-        
-        if (user.id === danielid) {
-            console.log("sending daniel img..")
-            msg.channel.send(danielImg)
-        }
-    });
-    
-})
-
-bot.login(process.env.token);
