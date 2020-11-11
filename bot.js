1325a195c1603e69aac6fab91072b56378521226
sifudiep <code.sifudiep@gmail.com>
2020-11-11 17:55:44 +0100

    combined nightmare and bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,33 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-import {imageURLS} from './nightmare.js';
+
+const Nightmare = require('nightmare')
+const nightmare = Nightmare();
+
+imageURLS = [];
+yValue = 0
+
+nightmare.goto("https://duckduckgo.com/?q=clown&atb=v231-1&iar=images&iax=images&ia=images")
+for (i = 0; i < 10; i++) {
+    yValue += 99999
+    nightmare.scrollTo(yValue, 0)
+    nightmare.wait(1000)
+}
+nightmare.evaluate(() => {
+        imgElements = document.body.getElementsByClassName("tile--img__img  js-lazyload");
+        imgURLS = []
+        for (let index = 0; index < imgElements.length; index++) {
+            imgURLS.push(imgElements[index].src)
+        }
+        return imgURLS;
+    })
+    .then(result => {
+        imageURLS = result;
+    })
+    .catch(error => {
+        console.error('Search failed:', error)
+    })
+nightmare.end();
 
 clownIndex = 0;
 
